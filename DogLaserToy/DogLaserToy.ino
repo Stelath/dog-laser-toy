a3640cd0513abe986060302a47427302111fd190
Alex <alex.k.korteacounts@gmail.com>
2020-07-11 23:49:21 -0400

    Arduino will now activate servos when RFID tag is active, needs calibration for servos

--- DogLaserToy/DogLaserToy.ino
+++ DogLaserToy/DogLaserToy.ino
@@ -1,9 +1,115 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+#define SERVO1PIN 2
+#define SERVO2PIN 3
 
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+// Setup servos
+
+bool active = false;
+
+// Set the laser wait time in milliseconds
+int laserWaitTime = 1000;
+
+// Setup a variable to record the last time the laser moved
+long timeOfLastMovement;
+
+Servo servo1;
+Servo servo2;
+
+long randNumber;
+
+// Function that points the laser at certain coordinates
+  void laserCoordinates(float xPos, float yPos)
+  {
+    servo1.write((-xPos * 1.8) + 90);
+    servo2.write(-yPos * 1.8);
+  }
+
+void setup() 
+{
+  // Create a random seed using the analog noise
+  randomSeed(analogRead(0));
+
+  // Initiate a serial communication
+  Serial.begin(9600);
+  mfrc522.PCD_Init();
+
+  // Attatch Servos
+  servo1.attach(SERVO1PIN);
+  servo2.attach(SERVO2PIN);
+
+  // Reset Servo Pos
+  laserCoordinates(0, 0);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  // If the robot is active move the laser to random positions, then wait for the specified laser wait time.
+  if (active)
+  {
+    if (laserWaitTime <= millis() - timeOfLastMovement) {
+      // Set random positions
+    float xRandPos = random(0, 101);
+    float yRandPos = random(0, 101);
 
-}
+    // Move the laser
+    laserCoordinates(xRandPos, yRandPos);
+
+    // Record time
+    timeOfLastMovement = millis();
+    Serial.println(timeOfLastMovement);
+    }
+  }
+  
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+
+  if (content.substring(1) == "D5 53 0D AB" || content.substring(1) == "0D 3A 80 CB") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+
+    if (!active)
+    {
+      active = true;
+    }
+    else
+    {
+      active = false;
+    }
+    delay(750);
+  }
+  else
+  {
+    Serial.println(" Access denied");
+    
+  }
+} 
